f088cdf0ad8be4a8513b6ce17b85047040475908
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-07-23 19:56:54 +0500

    Adding bank directory in Week 1

---
+++ Week 1/bank/bank.py
@@ -0,0 +1,8 @@
+b = input("Greeting: ").strip()
+
+if b == "Hello" or b == "Hello, Newman" or b =="hello there":
+    print("$0")
+elif b == "Hey" or b == "h" or b == "How you doing?":
+    print("$20")
+else:
+    print("$100")
