ae7544307f4e06ad95f5755b024c197b7170edf9
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-07-23 19:44:22 +0500

    Adding Week 1 Lectures's files

---
+++ Week 1/grade.py
@@ -0,0 +1,24 @@
+score = int(input("Score: "))
+
+if score >= 85:
+    print("Grade: A")
+elif score >= 81:
+    print("Grade: A-")
+elif score >= 75:
+    print("Grade: B+")
+elif score >= 70:
+    print("Grade: B")
+elif score >= 67:
+    print("Grade: B-")
+elif score >= 63:
+    print("Grade: C+")
+elif score >= 60:
+    print("Grade: C")
+elif score >= 57:
+    print("Grade: C-")
+elif score >= 53:
+    print("Grade: D+")
+elif score >= 50:
+    print("Grade: D")
+else:
+    print("Grade: F")
