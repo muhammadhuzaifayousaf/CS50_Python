b0c8ddb880f1e1bf34b5a2a4b7a7d92f048399b5
Muhammad Huzaifa Yousaf <mohammadhuzaifa63@gmail.com>
2024-08-18 22:07:46 +0500

    Adding Final Week 9 lecture' files. This was CS50

---
+++ Week 9/say.py
@@ -0,0 +1,12 @@
+# commented-out because cowsway and pyttsx3 library not working
+'''
+import cowsay
+import pyttsx3
+
+engine = pyttsx3.init()
+this = input("What's this? ")
+cowsay.cow(this)
+engine.say(this)
+engine.runAndWait()
+'''
+# This was CS50
